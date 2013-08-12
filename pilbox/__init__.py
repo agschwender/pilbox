1df9dc09769e215722f43a510fa27b125880ac9f
Adam Gschwender <adam.gschwender@gmail.com>
2013-08-12 19:10:31 -0400

    Removed unnecessary travis script commands

--- pilbox/__init__.py
+++ pilbox/__init__.py
@@ -30,10 +30,10 @@ Versions:
 """
 
 # human-readable version number
-version = "0.5.2"
+version = "0.5.3"
 
 # The first three numbers are the components of the version number.
 # The fourth is zero for an official release, positive for a development
 # branch, or negative for a release candidate or beta (after the base version
 # number has been incremented)
-version_info = (0, 5, 2, 0)
+version_info = (0, 5, 3, 0)
