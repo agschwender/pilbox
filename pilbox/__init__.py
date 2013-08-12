7e78fa05af61c99c3b02c9351df9851a52e04a53
Adam Gschwender <adam.gschwender@gmail.com>
2013-08-12 19:06:59 -0400

    Increased the version

--- pilbox/__init__.py
+++ pilbox/__init__.py
@@ -30,10 +30,10 @@ Versions:
 """
 
 # human-readable version number
-version = "0.5.1"
+version = "0.5.2"
 
 # The first three numbers are the components of the version number.
 # The fourth is zero for an official release, positive for a development
 # branch, or negative for a release candidate or beta (after the base version
 # number has been incremented)
-version_info = (0, 5, 1, 0)
+version_info = (0, 5, 2, 0)
